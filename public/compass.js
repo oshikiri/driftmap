5c182a314988382ae462762c46c5cefaf6351ffe
Takamasa Oshikiri <mail@oshikiri.org>
2025-08-20 00:43:40 +0900

    Add compass drawing functionality and initial HTML structure

---
+++ public/compass.js
@@ -0,0 +1,36 @@
+export function drawCompass(ctx, angle) {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // Draw outer circle
+  ctx.beginPath();
+  ctx.arc(100, 100, 90, 0, 2 * Math.PI);
+  ctx.strokeStyle = "#333";
+  ctx.lineWidth = 4;
+  ctx.stroke();
+
+  // Draw cardinal points
+  ctx.font = "20px sans-serif";
+  ctx.fillStyle = "#333";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("N", 100, 20);
+  ctx.fillText("E", 180, 100);
+  ctx.fillText("S", 100, 180);
+  ctx.fillText("W", 20, 100);
+
+  // Draw needle
+  ctx.save();
+  ctx.translate(100, 100);
+  ctx.rotate(((angle - 90) * Math.PI) / 180); // 0deg = North
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.lineTo(0, -70);
+  ctx.strokeStyle = "#e00";
+  ctx.lineWidth = 6;
+  ctx.stroke();
+  ctx.restore();
+}
+
+export function degToCardinal(deg) {
+  const dirs = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"];
+  return dirs[Math.round(deg / 45)];
+}
